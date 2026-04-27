22fcb4904a44115c92adfc24a4f6587b8227e94c
al16gm <alexgarciam91@gmail.com>
2026-04-27 18:10:49 +0200

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/colectores/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
